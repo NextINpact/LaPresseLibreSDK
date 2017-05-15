b88622b235fdc0b9ce941fca2aa9333521c18cc5
stephane@letsroot.net <stephane@letsroot.net>
2017-05-15 16:50:02 +0200

    Correction typo

--- php/models/InformationsCompteModel.php
+++ php/models/InformationsCompteModel.php
@@ -4,7 +4,7 @@
 * Modèle pour la création d'un lien d'inscription partenaire
 * Représente le modèle de données qui compose le paramètre <user> dans l'url d'inscription partenaire
 * @author  Bastien Caubet <bastien@nextinpact.com>, Luc Raymond <luc@nextinpact.com>
-* 
+*
 * The MIT License (MIT) Copyright (c) 2016 INpact Mediagroup
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -17,12 +17,12 @@ class InformationsCompteModel
 {
     public $Email;
     public $Pseudo;
-    public $Guid
+    public $Guid;
 
     public function __construct($email, $userName, $guid)
     {
         $this->Email = $email;
         $this->Pseudo = $userName;
-        $this->Guid = $guid
+        $this->Guid = $guid;
     }
 }
